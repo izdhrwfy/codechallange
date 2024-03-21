629a2dcac8cdc5896e2b06f1ddc8259cced04ae2
izdhrwfy <izdiharqatrunnada@gmail.com>
2024-03-21 21:44:41 +0700

    edit team

--- src/app/teams/components/team.ts
+++ src/app/teams/components/team.ts
@@ -3,7 +3,7 @@ export const OwnerVenanda = [
     nama: "Aina",
     Posisi: "Pemilik dan Trainer Venanda Bakery and Baking Class",
     deskripsi:
-      "Dengan 20 tahun pengalaman di dunia Bakery, 11 Tahun pengalaman mengajar dan sudah mencetak ribuan orang menjadi pengusaha bidang bakery di di Kudus, Pati, Jepara, Demak, Semarang, Purwodadi, Rembang, Lasem, Tuban, dan Kuwait sejak tahun 2010.",
+      "Dengan 20 tahun pengalaman di dunia Bakery, 11 Tahun pengalaman mengajar dan sudah mencetak ribuan orang menjadi pengusaha bidang bakery di Kudus, Pati, Jepara, Demak, Semarang, Purwodadi, Rembang, Lasem, Tuban, dan Kuwait sejak tahun 2010.",
     gambar: "/assets/owner.jpg",
   },
 ];
